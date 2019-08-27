fecddfd783ab73c5d682d6e37b8c47b9c01917b7
Pawel <pawel.zawadzki@onet.pl>
2019-08-27 19:15:54 +0100

    working passbraker

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,4 +1,8 @@
 package app;
 
 public class Main {
+    public static void main(String[] args) {
+        PassBraker passBraker = new PassBraker();
+        passBraker.passBraker("pablo");
+    }
 }
